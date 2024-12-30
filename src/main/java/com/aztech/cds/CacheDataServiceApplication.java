97ac1676c525277a26687e7747efa7a9eff3fdf9
Mukashfi <elmukashfi.hassan@gmail.com>
2024-12-30 21:33:31 +0400

    init cds-service-2@

---
+++ src/main/java/com/aztech/cds/CacheDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aztech.cds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheDataServiceApplication.class, args);
+	}
+
+}
